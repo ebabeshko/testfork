6820abaa9f7065727a74361d3df192a2bbec628f
mirexcool <mirexcool@gmail.com>
2022-12-07 18:47:34 +0200

    add time to my line
    Signed-off-by: mirexcool <mirexcool@gmail.com>

--- hello.c
+++ hello.c
@@ -9,7 +9,7 @@ int main()
 {
   printf("Andrii\n");
   myfunc();
-  printf("This is my BUG ! by yyefimov 18:35");
+  printf("This is my BUG ! by yyefimov 18:48");
 	printf("hey you\n");
   return 0;
 }
